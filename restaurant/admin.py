b304c51112e673258a50d135d1e6726ac5a95b9d
menghawj08 <menghawj@gmail.com>
2025-02-06 23:10:10 -0700

    Update admin.py

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Menu
-from .models import Booking
+from .models import Booking, 
 
-
+admin.site.register(Booking)
 admin.site.register(Menu)
